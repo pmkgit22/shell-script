ceb524841cdad19d96e07e04f01914838361ef5d
Munikumar <pmkdevops22@gmail.com>
2025-01-11 16:26:08 +0530

    this is test of shell

--- loopsinstallsoftwares.sh
+++ loopsinstallsoftwares.sh
@@ -5,7 +5,12 @@ N="\e[0m"
 B="\e[33m"
 echo "fuck"
 
-if [ $USERID -ne 0 ];
-then
-echo -e "$R this is in if condition"
+#!/bin/bash
+
+#userid=0  # Set userid to some value, for testing purposes.
+
+if [ "$USERID" -ne 0 ]; then
+  echo "User ID is not zero."
+else
+  echo "User ID is zero."
 fi
