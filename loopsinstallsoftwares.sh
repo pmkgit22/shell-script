d2fd46a23b54ec6a3d8dfdde392660fadc4ab83d
Munikumar <pmkdevops22@gmail.com>
2025-01-11 16:10:29 +0530

    this is test of shell

--- loopsinstallsoftwares.sh
+++ loopsinstallsoftwares.sh
@@ -4,9 +4,10 @@ R="\e[31m]"
 N="\e[0m]"
 B="\e[33m]"
 echo "the user id is this  :" $(id -u)
-if [ $USERID -ne 0]
+if [ $USERID -ne 0];
  then
   echo -e "$R ERROR: this is not super user"
 else
   echo -e " $B MESS: this is root user good"
 fi
+echo -e "$N Message :still continuing with out exit"
