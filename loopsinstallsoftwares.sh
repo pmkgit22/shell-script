414e12d1bbb2d203fb977a55b02aaadfee896f72
Munikumar <pmkdevops22@gmail.com>
2025-01-11 15:27:17 +0530

    Your shell script message

--- loopsinstallsoftwares.sh
+++ loopsinstallsoftwares.sh
@@ -4,6 +4,6 @@ R="\e[31m]"
 N="\e[0m]"
 if [$USERID -ne 0 ]
 then
-echo -e "$R Error: Please try to install with root access."
+echo -e "$R Error: :Please try to install with root access."
 exit 1
 fi
