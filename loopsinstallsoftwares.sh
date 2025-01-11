aed43b944679a8cd265c29b3443a9803cd621f25
Munikumar <pmkdevops22@gmail.com>
2025-01-11 15:51:53 +0530

    this is test of shell

--- loopsinstallsoftwares.sh
+++ loopsinstallsoftwares.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-USERID=$(id -u)
+USERID= $(id -u)
 R="\e[31m]"
 N="\e[0m]"
-if [$USERID -ne 0 ]
-then
-echo -e "$R Error::Please try to install with root access."
-exit 1
-fi
+echo "the user id is : $USERID"
