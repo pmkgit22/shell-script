af572ca9c7bc398d7e89e80ad52b1305f179ecea
Munikumar <pmkdevops22@gmail.com>
2025-01-12 13:33:26 +0530

    this is to run softwares

--- loopsinstallsoftwares.sh
+++ loopsinstallsoftwares.sh
@@ -3,15 +3,15 @@ USERID=$(id -u )
 DATE=$(date +%F)
 LOGDIR=/home/centos/shellscript-logs
 SCRIPT_NAME=$0
-LOGFILE=$LOGDIR/$SCRIPT_NAME-$DATE.log
+LOGFILE=$LOGDIR/$0-$DATE.log
 R="\e[31m"
 N="\e[0m"
-B="\e[33m"
+B="\e[34m"
 echo "The user Id is : $USERID"
 
 if [ $USERID -ne 0 ];
  then
-  echo -e "$R ERROR: this is not super user Run with super user"
+  echo -e "$R ERROR: this is not super user Run with super user$N"
   exit 1
  #else
 #echo -e " $B MESS: this is root user good"
@@ -20,10 +20,10 @@ fi
 Validate(){
  if [ $1 -ne 0 ];
   then
-  echo -e"$R $2 installation failed"
+  echo -e "$R $2 installation failed$N"
   exit 1
   else
-  echo -e"$B $2 succesfully installed"
+  echo -e "$B $2 succesfully installed$N"
 fi
 
 }
@@ -34,11 +34,11 @@ do
 
   if [ $? -ne 0 ];
   then
-  echo -e "$R $i Not installed $B Installing the software:"
+  echo -e "$R $i Not installed $B Installing the software:$N"
   yum install $i -y &>>$LOGFILE
   Validate $? "$i"
   else
-  echo -e "$R $i software is already installed"
+  echo -e "$R $i software is already installed$N"
 fi
- 
+
 done
