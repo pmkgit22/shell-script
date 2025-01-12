0a90d59618c4cce94ae0b21d3950465fe2d45351
Munikumar <pmkdevops22@gmail.com>
2025-01-12 14:33:57 +0530

    this is to delete old files

---
+++ Deleteold logfiles.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+APPLOG_FILES=/home/centos/app-logs
+USERID=$(id -u )
+DATE=$(date +%F)
+LOGDIR=/home/centos/shellscript-logs
+SCRIPT_NAME=$0
+LOGFILE=$LOGDIR/$0-$DATE.log
+R="\e[31m"
+N="\e[0m"
+B="\e[34m"
+FILES_TO_DELETE=$(find $APPLOG_FILES -name "*.log" -type f -mtime +14)
+
+echo " files to delete : $FILES_TO_DELETE"
