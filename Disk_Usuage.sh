dd7438d00530c9d5b3f2c9a4df0c736b7a3940e9
Munikumar <pmkdevops22@gmail.com>
2025-01-24 13:54:29 +0530

    this is disk usage

--- Disk_Usuage.sh
+++ Disk_Usuage.sh
@@ -11,11 +11,22 @@ Y="\e[33m"
 
 DISK_USAGE=$(df -hT | grep -vE 'tmpfs|Filesystem')
 DISK_USAGE_THRESHOLD=1
+Message=""
 
 # IFS is internal field seperator that is space
 
 while IFS= read line
  do
-  echo " the output is : $line"
+ # htis will give disk usage in number format for comaparasion
+ Usage=$( echo $line | awk '{print $6}' | cut -d % -f1)
+ #this is for partition
+ Partition=$( echo $line | awk '{print $1}' )
+ # this is to compare disk 
+   if [ $Usage -gt $DISK_USAGE_THRESHOLD ] ;
+    then
+     Message+="High disk usage alert $Partition  : $Usage "
+   fi   
 
  done <<<  $DISK_USAGE
+
+echo "message : $Message"
