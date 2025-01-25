0a38c818f041a9e10e37ebaf8e91a6e3d02da762
Munikumar <pmkdevops22@gmail.com>
2025-01-25 13:31:04 +0530

    this is mail fucking configuration

--- Disk_Usuage.sh
+++ Disk_Usuage.sh
@@ -31,4 +31,5 @@ while IFS= read line
 
 echo -e "message : $Message"
 #this will call other script for mail format
-sh mail.sh "pmk.kumar22@gmail.com" "High Disk Usage" "$Message" "Nani Team" " Disk Usage ALert"
+
+sh mail.sh "pmk.kumar22@gmail.com" "High Disk Usage" "$Message" "fucking Team" " Disk loaded Usage ALert"
--- mail.sh
+++ mail.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #echo "This is a test mail & Date $(date)" | mail -s "message" info@joindevops.com
 
+# Any one can project can call this script
+
 TO_ADRESS=$1
 SUBJECT=$2
 BODY=$3
@@ -8,3 +10,8 @@ TEAM_NAME=$4
 ALERT_TYPE=$5
 
 echo " ALL Arguments : $@"
+
+FINAL_BODY=$ ( sed -e "s/TeamName/$TEAM_NAME/g" -e "s/Alert_Type/$ALERT_TYPE/g" -e "s/MESSAGE/$BODY/g"  template.html)
+
+#echo "$BODY & Date $(date)" | mail -s "message" pmk.kumar22@gmail.com
+echo "$FINAL_BODY " | mail -s "$SUBJECT" $TO_ADRESS
